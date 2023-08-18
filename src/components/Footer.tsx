c8ad801ba9bd639c6dc370c84c2574b99046f8cc
kamilsz1916 <kamilsz1916@wp.pl>
2023-08-18 08:47:57 +0200

    forecast

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="footer">
             <a href="https://www.weatherapi.com/" title="Free Weather API"><img src='//cdn.weatherapi.com/v4/images/weatherapi_logo.png' alt="Weather data by WeatherAPI.com" /></a>
-            <a href="http://github.com/szulczyk">Copyright K.Sz. 2023</a>
+            <a href="http://github.com/szulczyk">Kamil Szulczyk</a>
         </footer>
     )
 }
