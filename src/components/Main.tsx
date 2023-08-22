6b69f4113e93ec04746de6b6610b049684c0ecbf
kamilsz1916 <kamilsz1916@wp.pl>
2023-08-22 22:12:09 +0200

    changing day works

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -13,6 +13,8 @@ import Map from './Map';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCity } from '@fortawesome/free-solid-svg-icons/faCity';
 import { faX } from '@fortawesome/free-solid-svg-icons/faX';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons/faChevronLeft';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons/faChevronRight';
 
 const Main = () => {
 
@@ -25,6 +27,9 @@ const Main = () => {
   const [locationDiv, setLocationDiv] = useState<any>();
   const [currentWeatherDiv, setCurrentWeatherDiv] = useState<any>();
   const [forecastWeatherDiv, setForecastWeatherDiv] = useState<any>();
+  const [forecastDay, setForecastDay] = useState<number>(0);
+  const [showPrevButton, setShowPrevButton] = useState<boolean>(false);
+  const [showNextButton, setShowNextButton] = useState<boolean>(false);
   const [hoursDiv, setHoursDiv] = useState<any>();
   const [mapCoords, setMapCoords] = useState<any>();
   const [mapDiv, setMapDiv] = useState<any>();
@@ -93,6 +98,7 @@ const Main = () => {
     if (weatherData === "" || weatherData === "undefined" || weatherData.length === 0) {
       setCurrentWeatherDiv("");
     } else {
+      setShowNextButton(true);
       setLocationDiv(
         <Location
           city={weatherData.location.name}
@@ -164,12 +170,61 @@ const Main = () => {
     }
   }, [mapCoords]);
 
-  function locMe() {
+  const locMe = () => {
     navigator.geolocation.getCurrentPosition(function (position) {
       const ltPlusLn = '' + position.coords.latitude + ',' + position.coords.longitude + '';
       setCoordinates(ltPlusLn);
     });
   }
+
+  useEffect(() => {
+    if (weatherData.forecast === "" || weatherData.forecast === undefined || weatherData.forecast.length === 0) {
+      setForecastWeatherDiv("");
+      setHoursDiv("")
+    } else {
+      setForecastWeatherDiv(
+        <Forecast
+          title={"Forecast"}
+          date={weatherData.forecast.forecastday[forecastDay].date}
+          icon={weatherData.forecast.forecastday[forecastDay].day.condition.icon}
+          header={weatherData.forecast.forecastday[forecastDay].day.condition.text}
+          maxTempC={weatherData.forecast.forecastday[forecastDay].day.maxtemp_c}
+          maxTempF={weatherData.forecast.forecastday[forecastDay].day.maxtemp_f}
+          minTempC={weatherData.forecast.forecastday[forecastDay].day.mintemp_c}
+          minTempF={weatherData.forecast.forecastday[forecastDay].day.mintemp_f}
+          avgTempC={weatherData.forecast.forecastday[forecastDay].day.avgtemp_c}
+          avgTempF={weatherData.forecast.forecastday[forecastDay].day.avgtemp_f}
+          maxWindMph={weatherData.forecast.forecastday[forecastDay].day.maxwind_mph}
+          maxWindKph={weatherData.forecast.forecastday[forecastDay].day.maxwind_kph}
+          rain={weatherData.forecast.forecastday[forecastDay].day.daily_will_it_rain}
+          rainChancePercent={weatherData.forecast.forecastday[forecastDay].day.daily_chance_of_rain}
+          snow={weatherData.forecast.forecastday[forecastDay].day.daily_will_it_snow}
+          snowChancePercent={weatherData.forecast.forecastday[forecastDay].day.daily_chance_of_snow}
+          avgHumidity={weatherData.forecast.forecastday[forecastDay].day.avghumidity}
+          uv={weatherData.forecast.forecastday[forecastDay].day.uv}
+          sunrise={weatherData.forecast.forecastday[forecastDay].astro.sunrise}
+          sunset={weatherData.forecast.forecastday[forecastDay].astro.sunset}
+        />);
+      setHoursDiv(weatherData.forecast.forecastday[forecastDay].hour.map((data: any) => (
+        <Hourly
+          time={data.time}
+          icon={data.condition.icon}
+          tempC={data.temp_c}
+          tempF={data.temp_f}
+          text={data.condition.text}
+        />)));
+      if (forecastDay === 2) {
+        setShowPrevButton(true);
+        setShowNextButton(false);
+      } else if (forecastDay === 1) {
+        setShowPrevButton(true);
+        setShowNextButton(true);
+      } else if (forecastDay === 0) {
+        setShowPrevButton(false);
+        setShowNextButton(true);
+      }
+    }
+  }, [forecastDay]);
 
   return (
     <div className="main">
@@ -182,6 +237,8 @@ const Main = () => {
       </div>
       <div className="data">{locationDiv}</div>
       <div className="data">{currentWeatherDiv}</div>
+      <a onClick={() => setForecastDay(forecastDay - 1)} style={{ visibility: showPrevButton == true ? 'visible' : 'hidden' }}><FontAwesomeIcon icon={faChevronLeft} /></a>
+      <a onClick={() => setForecastDay(forecastDay + 1)} style={{ visibility: showNextButton == true ? 'visible' : 'hidden' }}><FontAwesomeIcon icon={faChevronRight} /></a>
       <div className="data">{forecastWeatherDiv}</div>
       <div className="data">{hoursDiv}</div>
       <div className="data">{mapDiv}</div>
