6c761af670c59673248e9e012e3b87c1ff36e504
kamil301093 <kamilsz1916@wp.pl>
2023-11-23 20:14:04 +0100

    Updates

--- src/components/Nothing.tsx
+++
@@ -1,9 +0,0 @@
-const Nothing = (props: {nothingFunction:any}) => {
-    return (
-        <div className="nothing">
-            Nothing to show. Type city name or use <a className="nothing__link" onClick={props.nothingFunction}>geolocation.</a>
-        </div>
-    )
-}
-
-export default Nothing;
